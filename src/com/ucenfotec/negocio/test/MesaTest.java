931a311d59ccdf4a4ea497152184fa625305fc26
Jose Solano <dsolano@builderhomesite.com>
2018-06-16 01:17:05 -0600

    7. Agregar Jugador a la Mesa
    Se valida que solo puedan existir 4 jugadores en una mesa.
    Se valida la insercion correcta del jugador a la mesa

--- src/com/ucenfotec/negocio/test/MesaTest.java
+++ src/com/ucenfotec/negocio/test/MesaTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.ucenfotec.negocio.Jugador;
 import com.ucenfotec.negocio.Mesa;
 import com.ucenfotec.negocio.Repartidor;
 
@@ -29,6 +30,36 @@ public class MesaTest {
 		Repartidor nuevoRepartidor = new Repartidor();
 		mesa.setRepartidor(nuevoRepartidor);
 		assertTrue(mesa.getRepartidor() != null);
+	}
+	
+	@Test
+	public void AgregarJugador() throws Exception {
+		mesa =  new Mesa();
+		Jugador nuevoJugador = new Jugador();
+		mesa.agregarJugador(nuevoJugador);
+		assertEquals(1, mesa.getJugadores().size());
+	}
+	
+	@Test(expected = Exception.class)
+	public void AgregarMaxJugador() throws Exception {
+		mesa =  new Mesa();
+		
+		Jugador nuevoJugador = new Jugador();
+		mesa.agregarJugador(nuevoJugador);
+		
+		Jugador nuevoJugador2 = new Jugador();
+		mesa.agregarJugador(nuevoJugador2);
+
+		Jugador nuevoJugador3 = new Jugador();
+		mesa.agregarJugador(nuevoJugador3);
+		
+		Jugador nuevoJugador4 = new Jugador();
+		mesa.agregarJugador(nuevoJugador4);
+		
+		Jugador nuevoJugador5 = new Jugador();
+		mesa.agregarJugador(nuevoJugador5);
+		
+		assertEquals(4, mesa.getJugadores().size());
 	}
 
 }
